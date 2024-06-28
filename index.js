7d2c1543282c6697a4c2a4b50e2c114a88d02018
Ivo196 <ivo.tonioni@gmail.com>
2024-06-28 18:44:22 +0200

    add chat with llama3

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import express from "express"
 
 const app = express()
 
+const replicate = new Replicate({
+    auth: process.env.REPLICATE_API_KEY,
+});
 app.use(express.json())
 
 app.post("/generate", async (req, res) => {
@@ -14,9 +17,6 @@ app.post("/generate", async (req, res) => {
     }
 
 
-    const replicate = new Replicate({
-        auth: process.env.REPLICATE_API_KEY,
-    });
     
     console.log("Running the model...");
     
@@ -33,6 +33,31 @@ app.post("/generate", async (req, res) => {
       console.log(output);
     
     res.json(output)
+})
+
+app.post("/chat", async (req, res) => {
+    const prompt = req.body.prompt
+    if (!prompt) {
+        return res.status(400).json({error:"Prompt is required"})
+    }
+    const input = {
+        top_k: 0,
+        top_p: 0.9,
+        prompt: prompt,
+        max_tokens: 512,
+        min_tokens: 0,
+        temperature: 0.6,
+        length_penalty: 1,
+        prompt_template: "{prompt}",
+        presence_penalty: 1.15,
+        log_performance_metrics: false
+      };
+      
+      for await (const event of replicate.stream("meta/meta-llama-3-8b", { input })) {
+        //process.stdout.write(event.toString());
+        res.write(event.toString())
+      };
+      res.end()
 })
 
 app.listen(3000, () => {
